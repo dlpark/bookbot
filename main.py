132e9fa3f2ec6b6d427b12ab39f2131a59e294c7
dlpark <damienlevipark@gmail.com>
2025-01-01 21:02:55 +0000

    added ordered analysis of letter counts to main.py

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ def main():
     text_location = "books/frankenstein.txt"
     text = get_text(text_location)
     wordcount = get_wordcount(text)
-    charcount = get_char_count(text)
+    char_count = get_char_count(text)
+    ordered_char = get_char_order(char_count)
 
-    print(f"Analysing text in the file: {text_location}")
+    print(f"--- Analysing text in the file: {text_location} ---")
     print(f"This file contains {wordcount} words.")
-    print(charcount)
+    
+    for x in range(len(ordered_char)):
+        character = ordered_char[x]["character"]
+        count = ordered_char[x]["count"]
+        print(f"The letter '{character}' appears {count} times.")
+    
+    print("--- End of analysis ---")
 
 def get_text(filepath):
     with open(filepath) as f:
@@ -31,5 +38,20 @@ def get_char_count(input_text):
         else:
             letters[letter] = 1
     return letters
+
+def sort_on(dict):
+    return dict["count"]
+
+def get_char_order(char_dict):
+    # Create a list to store dictionaries
+    sorted_char_list = []
+    # Iterate through key-value pairs of dictionary items and make each into its own dictionary in the list
+    for char, count in char_dict.items():
+        entry = {'character': char, 'count': count}
+        if char.isalpha():
+            sorted_char_list.append(entry)
+    # Sort the list by reverse count order
+    sorted_char_list.sort(reverse=True, key=sort_on)
+    return sorted_char_list   
 
 main()
